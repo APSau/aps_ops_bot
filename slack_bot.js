2063b68545847b7658e49c18f3508af313baac2b
Ubuntu <ubuntu@ip-172-31-0-102.ap-southeast-2.compute.internal>
2018-09-07 05:28:14 +0000

    updates

---
+++ slack_bot.js
@@ -0,0 +1,153 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+           ______     ______     ______   __  __     __     ______
+          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
+          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
+           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
+            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
+
+
+This is a sample Slack bot built with Botkit.
+
+This bot demonstrates many of the core features of Botkit:
+
+* Connect to Slack using the real time API
+* Receive messages based on "spoken" patterns
+* Reply to messages
+* Use the conversation system to ask questions
+* Use the built in storage system to store and retrieve information
+  for a user.
+
+# RUN THE BOT:
+
+  Get a Bot token from Slack:
+
+    -> http://my.slack.com/services/new/bot
+
+  Run your bot from the command line:
+
+    token=<MY TOKEN> node slack_bot.js
+
+# USE THE BOT:
+
+  Find your bot inside Slack to send it a direct message.
+
+  Say: "Hello"
+
+  The bot will reply "Hello!"
+
+  Say: "who are you?"
+
+  The bot will tell you its name, where it is running, and for how long.
+
+  Say: "Call me <nickname>"
+
+  Tell the bot your nickname. Now you are friends.
+
+  Say: "who am I?"
+
+  The bot will tell you your nickname, if it knows one for you.
+
+  Say: "shutdown"
+
+  The bot will ask if you are sure, and then shut itself down.
+
+  Make sure to invite your bot into other channels using /invite @<my bot>!
+
+# EXTEND THE BOT:
+
+  Botkit has many features for building cool and useful bots!
+
+  Read all about it here:
+
+    -> http://howdy.ai/botkit
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+var Client = require('node-wolfram');
+var Wolfram = new Client('V3V6QU-LXXRWJYRXY');
+
+var cleverbot = require("cleverbot.io"),  
+cleverbot = new cleverbot('9P2WTbRCVMF5nqdu', 'x8fk7bB8DYqSajIVfn07mLQ1QTvVQObh');  
+cleverbot.setNick("bob");  
+cleverbot.create(function (err, session) {  
+    if (err) {
+        console.log('cleverbot create fail.');
+    } else {
+        console.log('cleverbot create success.');
+    }
+});
+
+if (!process.env.token) {
+    console.log('Error: Specify token in environment');
+    process.exit(1);
+}
+
+var Botkit = require('./lib/Botkit.js')  
+var os = require('os');
+
+var controller = Botkit.slackbot({  
+    debug: false,
+});
+
+var bot = controller.spawn(  
+  {
+      token:process.env.token
+  }
+).startRTM();
+
+
+controller.hears('wolfram','direct_message,ambient,direct_mention',function(bot,message) {
+
+var wquery = message.text;
+
+Wolfram.query(wquery, function(err, result) {
+    if(err)
+        console.log(err);
+    else
+    {
+        for(var a=0; a<result.queryresult.pod.length; a++)
+        {
+            var pod = result.queryresult.pod[a];
+            console.log(pod.$.title,": ");
+            for(var b=0; b<pod.subpod.length; b++)
+            {
+                var subpod = pod.subpod[b];
+                for(var c=0; c<subpod.plaintext.length; c++)
+                {
+                    var text = subpod.plaintext[c];
+                    console.log('\t', text);
+                }
+            }
+        }
+    }
+});
+
+
+
+
+});
+
+
+controller.hears('uptime','direct_message,ambient,direct_mention',function(bot,message) {
+    bot.reply(message,"how the hell would i know?");
+});
+
+controller.hears('josh','direct_message,ambient,direct_mention',function(bot,message) {
+    bot.reply(message,"did someone mention my god's name?");
+});
+
+
+
+controller.hears('','direct_message,ambient,direct_mention,mention',function(bot,message) {  
+    var msg = message.text;
+    cleverbot.ask(msg, function (err, response) {
+        if (!err) {
+            bot.reply(message, response);
+	    console.log(msg);
+	    console.log(response);
+        } else {
+            console.log('cleverbot err: ' + err);
+        }
+    });
+})
+
